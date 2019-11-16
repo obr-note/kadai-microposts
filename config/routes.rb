f70fbb1df038516b106a71949abdd4783a920404
obr.note <k33mdukb9n@yahoo.co.jp>
2019-11-16 15:58:44 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,11 @@ Rails.application.routes.draw do
         member do
             get :followings
             get :followers
+            get :likes
         end
     end
     
     resources :microposts, only: [:create, :destroy]
     resources :relationships, only: [:create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :favorites, only: [:create, :destroy]
 end
